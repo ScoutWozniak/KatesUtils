adb79eee1bc9132d0e20838af148183681c28885
ScoutWozniak <99102549+ScoutWozniak@users.noreply.github.com>
2026-02-18 13:15:45 +1000

    Simple free cam

--- Assets/Scripts/Examples/FreeCam.cs
+++ Assets/Scripts/Examples/FreeCam.cs
@@ -3,12 +3,51 @@ using UnityEngine;
 public class FreeCam : MonoBehaviour
 {
     public float mouseSensitivity = 0.02f;
+
+    public float moveSpeed = 10.0f;
+    
+    public KeyCode toggleLooking = KeyCode.F;
+
+    private bool _looking;
+    private bool Looking
+    {
+        get => _looking;
+        set {
+            _looking = value;
+            UpdateLooking(value);
+        }
+    }
+
+    void Start()
+    {
+        Looking = true;
+    }
+    
     // Update is called once per frame
     void Update()
     {
-        float newRotX = transform.localEulerAngles.x + Input.GetAxis("Mouse X") * mouseSensitivity;
-        float newRotY = transform.localEulerAngles.y + Input.GetAxis("Mouse Y") * mouseSensitivity;
-        transform.localEulerAngles = new Vector3(newRotX, newRotY, 0);
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Looking = !Looking;
+        }
+
+        transform.position += (transform.right * (Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime));
+        
+        transform.position += (transform.forward * (Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime));
+
+        if (Looking)
+        {
+            float newRotX = transform.localEulerAngles.x - Input.GetAxis("Mouse Y") * mouseSensitivity;
+            float newRotY = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * mouseSensitivity;
+            transform.localEulerAngles = new Vector3(newRotX, newRotY, 0);
+        }
         
     }
+
+    void UpdateLooking(bool state)
+    {
+        Cursor.visible = !state;
+        Cursor.lockState = state ? CursorLockMode.Locked : CursorLockMode.None;
+    }
+    
 }
